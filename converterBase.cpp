4265947ac04bc47f536095b99fd510b904d6d44b
fernandowallacehb <55742835+fernandowallacehb@users.noreply.github.com>
2020-02-14 08:45:28 -0300

    Update converterBase.cpp

--- converterBase.cpp
+++ converterBase.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 int converter10(std::string num, int baseO);
-int converterBase(std::string num, int baseO);
+std::string converterBase(std::string num, int baseO);
 
 int main(){
 
@@ -19,16 +19,15 @@ int main(){
 	std::cout<<"Base de saida: ";
 	std::cin>>baseO;
 
-	int buf = converter10(num, baseI);
-
 	if (baseI <= 1 || baseO <= 1){
 		std::cout<<"base invalida\n";
-		return;
+		return 0;
 	}
 
+	int buf = converter10(num, baseI);
 
 	if (baseO == 10)
-		std::cout<<buf <<"\n";
+		std::cout<< buf <<"\n";
 	
 	else{
 		std::cout<<converterBase(std::to_string(buf), baseO) <<"\n";
@@ -36,30 +35,40 @@ int main(){
 
 }
 
-
 int converter10(std::string num, int baseI){
 	int buf = 0;
 	std::string::iterator it = num.end() - 1;
 	int exp = 0;
-
+	
 	while(it != num.begin() - 1){
+		if(*it>64&&*it<91)
+		buf += ((*it) - 55) * pow(baseI, exp);
+		else
 		buf += ((*it) - 48) * pow(baseI, exp);
 		exp++;
 		it--;
 	}
+
 	return buf;
 }
 
 
-int converterBase(std::string num, int baseO){
+std::string converterBase(std::string num, int baseO){
 	int quo = stoi(num);
 	std::string buf;
-	int resto = 0;	
+	int resto = 0;
+	char conv;
 	while (quo != 0){
 		resto = quo % baseO;
-		quo /= baseO;		
+		quo /= baseO;
+		if(resto<10)		
 		buf.insert(0, std::to_string(resto));
+		else{
+		conv = resto + 55;	
+		buf.insert(0, 1, conv);
+		}
 	}
-	return stoi(buf);
-
+	
+	
+	return buf;
 }
